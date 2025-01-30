679d1054fb8de7179ab96d5b0f5f6785b63f080f
aski anaan <askianaan007@gmail.com>
2025-01-30 10:31:08 +0530

    basic controller created

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,3 @@
+const protectRoute = async (req, res, next) => {
+    
+};
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoute from "./routes/auth.route.js";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port number ${PORT}`);
+  connectDB();
+}); 
